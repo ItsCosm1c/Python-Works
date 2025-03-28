c3512d4cf8758db295ce8d4d7e0b0815a9a22c85
ItsCosm1c <108019252+ItsCosm1c@users.noreply.github.com>
2025-03-28 18:05:59 -0500

    Add files via upload

---
+++ Matthew Henry Python Work.py
@@ -0,0 +1,38 @@
+#Week 12 Programming Homework
+#Badge C-10 (Class)
+
+class Monster:      #C-10
+    def __init__(self, name, monster_type):
+        self.__name = name
+        self.__monster_type = monster_type
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_monster_type(self, monster_type):
+        self.__monster_type = monster_type
+
+    def get_name(self):
+        return self.__name
+    
+    def get_type(self):
+        return self.__monster_type
+    
+    def scare(self):
+        if self.__monster_type == "Frankenstein":
+            print("Fire Bad!")
+        elif self.__monster_type == "Werewolf":
+            print("Awoooo!")
+        elif self.__monster_type == "Dracula!":
+            print("Bleh!")
+        else:
+            print("Boo!")
+
+monster1 = Monster("Franky", "Frankenstein")
+monster2 = Monster("Drayden", "Dracula")
+
+print(monster1.get_name())
+print(monster2.get_name())
+
+monster1.scare()
+monster2.scare()
