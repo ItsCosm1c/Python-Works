c977c43d172e32b6f1d82432295c389276d79dba
ItsCosm1c <108019252+ItsCosm1c@users.noreply.github.com>
2025-03-28 18:16:57 -0500

    Update Matthew Henry Python Work #3.py

--- Matthew Henry Python Work #3.py
+++ Matthew Henry Python Work #3.py
@@ -4,13 +4,13 @@
 #Badge C3-3 (Comparisons)
 
 password1 = input("Enter your password to get in: \n>").lower().strip()
-password2 = input("Enter another password: \n>").lower().strip()        #(C3-3)nol
+password2 = input("Enter another password: \n>").lower().strip()    
 
-if(password1 == "knight" and password2 == "nolan"):             #(C3-1)
+if(password1 == "knight" or password2 == "nolan"): 
     print("You are now watching the movie 'The Dark Knight'.")
-elif(password1 == "chamalet" and password2 == "zendaya"):       
+elif(password1 == "chamalet" or password2 == "zendaya"):       
     print("You are now watching the movie 'Dune Part 2'.")
-elif(password1 == "morales" and password2 == "multiverse"):     #(C3-2)
+elif(password1 == "morales" or password2 == "multiverse"): 
     print("You are now watching the move 'Spiderman Across the Spiderverse'.")
 else:
     print("You don't deserve to be here, get out.")
