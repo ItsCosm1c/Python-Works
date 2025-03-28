88d746c17d73f51c3fb10b004b8a62178d9af817
ItsCosm1c <108019252+ItsCosm1c@users.noreply.github.com>
2025-03-28 18:15:41 -0500

    Update Matthew Henry Python Work #10.py

--- Matthew Henry Python Work #10.py
+++ Matthew Henry Python Work #10.py
@@ -1,6 +1,3 @@
-#Week 9 Homework
-#Badge C7-1 (Random)
-
 import random
 
 random_name = input('Please enter a Villager Name:\n>> ')
@@ -8,7 +5,7 @@ random_name = input('Please enter a Villager Name:\n>> ')
 personalities = ['Normal', 'Lazy', 'Sisterly', 'Snooty', 'Cranky', 'Jock','Peppy', 'Smug']
 hobbies = ['Education', 'Fashion', 'Fitness', 'Music', 'Nature', 'Playing']
 
-per_num = random.randint(0, len(personalities) - 1) #(C7-1)
+per_num = random.randint(0, len(personalities) - 1) 
 hob_num = random.randint(0, len(hobbies) - 1)
 
 per = personalities[per_num]
